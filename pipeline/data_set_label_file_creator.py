36d714424cefbcdd916fddbe9b9ea2192bb805d4
Alexandre Rangom <alexandre.rangom@gmail.com>
2020-02-27 09:42:37 +0100

    <feat>(<data_set_label_file_creator.py,>): <Implementation du support de multi bbox par image> <Implementation du support de multi bbox par image a travers la mise en place d'une liste de bbox, l'algo devient plus lent> <pas de footer>

--- pipeline/data_set_label_file_creator.py
+++ pipeline/data_set_label_file_creator.py
@@ -1,7 +1,8 @@
 import json
 from progressbar import ProgressBar
+import pdb
 
-with open("C:\dataset\COCO dataset\\annotations\instances_val2014.json") as json_data:
+with open(r"C:\dataset\COCO dataset\annotations\instances_train2014.json") as json_data:
     data_dict = json.load(json_data)
     data_set = []
 
@@ -10,21 +11,29 @@ with open("C:\dataset\COCO dataset\\annotations\instances_val2014.json") as json
 
     images_info = data_dict['images']
     images_annotation = data_dict['annotations']
-    print(images_info[0]['id'])
+    print(images_info[0])
+    print(images_annotation[0])
+
+    # pdb.set_trace()
 
     with ProgressBar(max_value=len(images_info)) as bar:
         i = 0
         for x in images_info:
             elements = None
-            id = x['id']
+            image_id = x['id']
             i += 1
             bar.update(i)
 
+            data = []
             for y in images_annotation:
-                if y['id'] == id:
-                    data_set.append({'id': id, 'file_name': x['file_name'], 'category_id': y['category_id'], 'bbox': y['bbox']})
-                    break
+                if y['image_id'] == image_id:
+                    data.append({'category_id': y['category_id'], 'bbox': y['bbox']})
 
-    with open('C:\dataset\COCO dataset\\annotations\labels\labels_val.json', 'a') as file_labels:
+            # pdb.set_trace()
+            data_set.append({'id': image_id, 'file_name': x['file_name'], 'height': x['height'], 'width': x['width'],
+                             'object_detection': data})
+            data = None
+
+    with open(r'labels_files/labels train.json', 'a') as file_labels:
         for data in data_set:
             file_labels.write(json.dumps(data))
